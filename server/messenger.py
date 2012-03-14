ea5af8766746af0a9a3e1bdc7389381847937f53
Steven Allen <steven@stebalien.com>
2012-03-13 20:49:06 -0400

    Update docs and fix getCount and getInstances.

--- server/messenger.py
+++ server/messenger.py
@@ -17,7 +17,7 @@ def make_message(sender, message, cls=None, instance=None, user=None):
         "message": message,
         "timestamp": time.time(),
         "read": False,
-        "class": cls or "message",
+        "cls": cls or "message",
         "instance": instance or "personal",
         "user": user
     }
@@ -36,9 +36,9 @@ def open_or_create_db():
     except IOError:
         if not os.path.isdir(settings.DATA_DIR):
             os.makedirs(settings.DATA_DIR)
-        db.create("sender", "message", "timestamp", "read", "class", "instance", "user")
+        db.create("sender", "message", "timestamp", "read", "cls", "instance", "user")
         db.create_index("sender")
-        db.create_index("class")
+        db.create_index("cls")
         db.create_index("read")
         db.create_index("instance")
         db.create_index("user")
@@ -109,6 +109,23 @@ class Messenger(object):
     @exported
     @return_status
     def send(self, message, cls=None, instance=None, user=None):
+        """
+        Send a zephyr.
+
+        Arguments:
+            message - The message content
+            cls - the message class (defaults to "message")
+            instance - the message instance (defaults to "personal")
+            user - the destination user (None means everyone)
+
+        # Send a message to -c help -i linux
+        >>> messenger.send("This is a really short message.", "help", "linux")
+
+        # Send a message to bsw (the two following are equivalent)
+        >>> messenger.send("This is a really short message.", None, None, "bsw")
+        >>> messenger.send("This is a really short message.", "message", "personal", "bsw")
+        """
+
         if self.submanager.matchTripplet(cls, instance, user):
             self.messages.insert(**make_message(self.username, message, cls, instance, user))
             self.messages.commit()
@@ -116,7 +133,28 @@ class Messenger(object):
     @exported
     def filterMessages(self, messageFilters, parent_fid=None):
         """
-        Takes a list of chained filters.
+        Filters messages.
+        Arguments:
+            messageFilters - a list of filters
+            parent_fid - the parent filters filter id
+
+        Returns:
+            fid - a string filterID
+
+        filter:
+            A list of ORd queries in the form of.
+            Query: (String(field), Bool(value_is_regex), value)
+            Filter: [QueryA, QueryB, QueryC] == QueryA v QueryB v QueryC
+
+        # Matches all unread messages in either the message or help class.
+        >>> fid = messenger.filterMessages([[("cls", False, "message"), ("cls", False, "help")], [("read", False, False)]])
+        # Get the messages that match the filter
+        >>> messenger.get(fid)
+
+        # Equivalent filter
+        >>> fidA = messenger.filterMessages([[("cls", False, "message"), ("cls", False, "help")]])
+        >>> fidB = messenger.filterMessages([[("read", False, False)]], fidA)
+        >>> messenger.get(fid)
         """
         f = self.filters[int(parent_fid)] if parent_fid is not None else None
         for messageFilter in messageFilters:
@@ -131,19 +169,49 @@ class Messenger(object):
 
     @exported
     def get(self, fid=None, start=0, end=float("inf")):
+        """
+        Get the messages that match fid.
+        Arguments:
+            fid - the filter's ID
+            start - the id of the first message (inclusive)
+            end - the id of the last message (*EXCLUSIVE*)
+        
+        returns a time sorted list of message dictionaries
+        Message format:
+
+        >>> {
+        >>>     "__id__": int,      # this is the id of the message
+        >>>     "__version__": int, # this is updated every time the message is changed (marked etc.)
+        >>>     "cls": str,
+        >>>     "instance": str,
+        >>>     "message": str,
+        >>>     "user": str,        # the destination user
+        >>>     "sender": str       # the sender
+        >>> }
+
+        """
         return sorted((self.messages[i] for i in self.applyFilter(fid, start, end)), key=lambda x: x["timestamp"])
 
     @exported
     def hasNew(self, last, fid=None):
-        self.applyFilter(fid=fid, start=last)
-        return (last+1) < self.messages.next_id
+        """ Returns true if there is a new message that matches the given filter. """
+        # XXX: This should be faster.
+        if fid is None:
+            return self.messages.next_id - 1 > last
+        else:
+            for i in self.applyFilter(fid=fid, start=last):
+                if i > last:
+                    return True
+            return False
 
     @exported
     def markFilter(self, status, fid=None, start=0, end=float("inf")):
+        """ Mark all of the messages that match a filter with the given status. """
         return self.mark(status, self.applyFilter(fid, start, end))
 
     @exported
     def delete(self, ids):
+        """ Delete the messages with th given ids. """
         self.messages.delete(self.messages[i] for i in ids)
         self.messages.commit()
 
@@ -156,12 +224,14 @@ class Messenger(object):
 
     @exported
     def deleteFilter(self, fid=None, start=0, end=float("inf")):
+        """ Delete the messages that match the given filter. """
         return self.delete(self.applyFilter(fid, start, end))
 
     @exported
     #@return_status
     def mark(self, status, ids):
-        raise NotImplementedError("This doesn't work for an unknown reason")
+        """ Mark the given messages as read. """
+        #raise NotImplementedError("This doesn't work for an unknown reason")
         messages = (self.messages[i] for i in ids)
         if status == "read":
             self.messages.update(messages, read=True)
@@ -173,19 +243,46 @@ class Messenger(object):
 
     @exported
     def getIDs(self, fid=None, start=0, end=float("inf")):
+        """ Get all of the message ids that match the given filter. """
         return sorted(self.applyFilter(fid, start, end))
         
 
     @exported
     def getClasses(self, unread=True):
-        return self.messages._class.keys()
+        """ List the classes with messages. """
+        return self.messages._cls.keys()
 
     @exported
-    def getInstances(self, cls, unread=True):
-        return tuple(set(m["instance"] for m in self.messages._class[cls]))
+    def getInstances(self, cls):
+        """ List the instances with messages in a given class.
+
+        returns:
+            [("instance", [unread_count, read_count]), ...]
+        """
+        insts = {}
+        for m in self.messages(cls=cls):
+            insts.setdefault(m["instance"], [])[int(m["read"])] += 1
+
+        return insts.items()
+
+    def getUnreadInstances(self, cls):
+        """ List the instances with unread messages in a given class.
+
+        returns:
+            [("instance", message_count), ...]
+        """
+        insts = {}
+        for m in self.messages(cls=cls, read=False):
+            i = m["instance"]
+            if i not in insts:
+                insts[i] = 1
+            else:
+                insts[i] += 1
+        return insts.items()
 
     @exported
     def getCount(self, fid=None):
+        """ Get the number of messages that match a filter. """
         if fid is None:
             return len(self.messages)
         else:
