87da7c6fd678ee9fa0f0ab40ce7799c6eb59ee55
Steven Allen <steven@stebalien.com>
2012-04-01 21:08:13 -0400

    Add true subscription management. (reads and writes to .zephyr.subs)

--- server/settings.py
+++ server/settings.py
@@ -1,5 +1,5 @@
 import os
 
 CONFIG_FILE = os.path.join(os.environ.get("XDG_CONFIG_HOME", os.path.expandvars("$HOME/.config")), "zephyr-server.ini")
-DATA_DIR = os.path.join(os.environ.get("XDG_DATA_HOME", os.path.expandvars("$HOME/.local/share")), "zephyr-server")
-ZEPHYR_DB = os.path.join(DATA_DIR, "zephyrs.db")
+ZEPHYR_DB = os.path.join(os.environ.get("XDG_DATA_HOME", os.path.expandvars("$HOME/.local/share")), "zephyr-server/zephyrs.db")
+ZSUBS = os.path.join(os.environ.get("HOME"), ".zephyr.subs")
