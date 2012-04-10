62ed631d16d25bad67bc453255badf23f91da8ca
Steven Allen <steven@stebalien.com>
2012-04-10 03:31:45 -0400

    Update and add tests.

--- zephyrserver/tests.py
+++ zephyrserver/tests.py
@@ -1,10 +1,12 @@
 import unittest
+#import test_zephyr as zephyr
 
 class testMessenger(unittest.TestCase):
 
     def setUp(self):
-        from messenger import Messenger
-        self.messenger = Messenger("ME", ":memory:")
+        import messenger
+        messenger.preferences = {"starred-classes": ["help"], "signature": ""}
+        self.messenger = messenger.Messenger("ME", ":memory:")
 
     def testStoreMessages(self):
         from datetime import datetime
@@ -20,21 +22,27 @@ class testMessenger(unittest.TestCase):
 
         self.assertEqual(m1, {
             "sender": "SENDER",
+            'signature': '',
             "message": "MESSAGE",
             "cls": "CLASS",
+            "signature": "",
             "instance": "INSTANCE",
             "user": "USER",
             "id": 1,
+            "auth": True,
             "read": False
         })
 
         self.assertEqual(m2, {
             "sender": "SENDER",
+            'signature': u'',
             "message": "MESSAGE2",
             "cls": "CLASS",
             "instance": "INSTANCE",
+            "signature": "",
             "user": "USER",
             "id": 2,
+            "auth": True,
             "read": False
         })
 
@@ -45,7 +53,9 @@ class testMessenger(unittest.TestCase):
             ("bsw", "Second linux message.", "help", "linux", None),
             ("bsw", "Linux.d message.", "help", "linux.d", None),
             ("bsw", "Other help message", "help", "other", None),
-            ("bsw", "User Message", "help", "other", None)
+            ("bsw", "User Message", "help", "other", None),
+            ("bsw", "M1", "message", "personal", "ME"),
+            ("steb", "M2", "message", "personal", "ME")
         )
 
     def timedPopulateTestMessages(self):
@@ -84,7 +94,7 @@ class testMessenger(unittest.TestCase):
         self.assertIsInstance(fid, str)
         msgs = self.messenger.get(fid)
         self.cleanResponse(msgs)
-        self.assertEquals(msgs, {
+        tst = {
             'filter': fid,
             'messages': [
                 {'cls': u'help',
@@ -92,34 +102,45 @@ class testMessenger(unittest.TestCase):
                  'message': u'First linux message.',
                  'read': False,
                  'sender': u'bsw',
+                 'signature': u'',
+                 "auth": True,
                  'user': None},
                 {'cls': u'help',
                  'instance': u'linux',
                  'message': u'Second linux message.',
                  'read': False,
                  'sender': u'bsw',
+                 'signature': u'',
+                 "auth": True,
                  'user': None},
                 {'cls': u'help',
                  'instance': u'linux.d',
                  'message': u'Linux.d message.',
                  'read': False,
                  'sender': u'bsw',
+                 'signature': u'',
+                 "auth": True,
                  'user': None},
                 {'cls': u'help',
                  'instance': u'other',
                  'message': u'Other help message',
                  'read': False,
                  'sender': u'bsw',
+                 'signature': u'',
+                 "auth": True,
                  'user': None},
                 {'cls': u'help',
                  'instance': u'other',
                  'message': u'User Message',
+                 'signature': u'',
                  'read': False,
                  'sender': u'bsw',
+                 "auth": True,
                  'user': None}],
             'offset': 0,
             'perpage': -1
-        })
+        }
+        self.assertEquals(msgs, tst)
 
     def testFilterMessagesMessage(self):
         self.populateTestMessages()
@@ -136,17 +157,23 @@ class testMessenger(unittest.TestCase):
                  'message': u'First linux message.',
                  'read': False,
                  'sender': u'bsw',
+                 'signature': u'',
+                 "auth": True,
                  'user': None},
                 {'cls': u'help',
                  'instance': u'linux',
                  'message': u'Second linux message.',
                  'read': False,
                  'sender': u'bsw',
+                 'signature': u'',
+                 "auth": True,
                  'user': None},
                 {'cls': u'help',
                  'instance': u'linux.d',
                  'message': u'Linux.d message.',
                  'read': False,
+                 'signature': u'',
+                 "auth": True,
                  'sender': u'bsw',
                  'user': None},],
             'offset': 0,
@@ -168,12 +195,16 @@ class testMessenger(unittest.TestCase):
                  'message': u'First linux message.',
                  'read': False,
                  'sender': u'bsw',
+                 'signature': u'',
+                 "auth": True,
                  'user': None},
                 {'cls': u'help',
                  'instance': u'linux',
                  'message': u'Second linux message.',
                  'read': False,
                  'sender': u'bsw',
+                 'signature': u'',
+                 "auth": True,
                  'user': None},],
             'offset': 0,
             'perpage': -1
@@ -194,17 +225,64 @@ class testMessenger(unittest.TestCase):
                  'message': u'Second linux message.',
                  'read': False,
                  'sender': u'bsw',
+                 'signature': u'',
+                 "auth": True,
                  'user': None},
                 {'cls': u'help',
                  'instance': u'linux.d',
                  'message': u'Linux.d message.',
                  'read': False,
                  'sender': u'bsw',
+                 'signature': u'',
+                 "auth": True,
                  'user': None},],
             'offset': 0,
             'perpage': -1
         })
 
+    def testGetClasses(self):
+        self.populateTestMessages()
+        self.assertEquals(self.messenger.getClasses(),
+                    [
+                        {'cls': u'help', 'starred': True, 'total': 5, 'unread': 5},
+                        {'cls': u'message', 'starred': False, 'total': 2, 'unread': 2},
+                        {'cls': u'offtopic', 'starred': False, 'total': 1, 'unread': 1},
+                    ]
+                   )
+
+    def testGetInstances(self):
+        self.populateTestMessages()
+        self.assertEquals(self.messenger.getInstances("help"),
+                    [
+                        {'instance': u'linux', 'total': 2, 'unread': 2},
+                        {'instance': u'linux.d', 'total': 1, 'unread': 1},
+                        {'instance': u'other', 'total': 2, 'unread': 2}
+                    ]
+                   )
+
+    def testGetPersonals(self):
+        self.populateTestMessages()
+        self.assertEquals(self.messenger.getPersonals(),
+                    [
+                        {'sender': u'bsw', 'total': 1, 'unread': 1},
+                        {'sender': u'steb', 'total': 1, 'unread': 1},
+                    ]
+                   )
+
+    def testMarkReadUnread(self):
+        self.populateTestMessages()
+        fid = self.messenger.filterMessages({"sender": "bsw"})
+        self.messenger.markFilterRead(fid, 1, 2)
+        self.assertTrue(not self.messenger.get(fid, 0, 1)["messages"][0]["read"])
+        self.assertTrue(all( i["read"] for i in self.messenger.get(fid, 1, 2)["messages"]))
+        self.assertTrue(all( not i["read"] for i in self.messenger.get(fid, 3)["messages"]))
+        self.messenger.markFilterUnread(fid, 1, 2)
+        self.assertTrue(all( not i["read"] for i in self.messenger.get(fid)["messages"]))
+
+    def testDelete(self):
+        fid = self.messenger.filterMessages({"sender": "bsw"})
+        self.messenger.deleteFilter(fid)
+        self.assertTrue(len(self.messenger.get(fid)["messages"]) == 0)
 
 
 if __name__ == '__main__':
