bf21f2ff6c3780bf36b74e11370d56f0b1e425f7
Steven Allen <steven@stebalien.com>
2012-04-13 15:38:00 -0400

    Allow variable PYTHON to override python binary.

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+PYTHON=${PYTHON:-python}
+
 export PYTHONPATH="$(dirname ${BASH_SOURCE[0]})/python-zephyr/"
-python -m zephyrserver.xmlrpc "${1:-localhost}" ${2}
+$PYTHON -m zephyrserver.xmlrpc "${1:-localhost}" ${2}
